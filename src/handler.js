0e0e6ee9ad6b7fdcea070088363694cee3fb04d3
blackflux <blackflux@unfine.de>
2018-08-12 22:27:04 -0700

    feat: simple logic when air quality level changes

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,8 @@
+const fs = require("fs");
+const zlib = require("zlib");
 const request = require("request-promise-native");
+const AWS = require("aws-sdk");
+const get = require("lodash.get");
 const rollbar = require('lambda-rollbar')({
   verbose: process.env.ROLLBAR_VERBOSE === "1",
   enabled: !!process.env.ROLLBAR_ACCESS_TOKEN,
@@ -6,11 +10,52 @@ const rollbar = require('lambda-rollbar')({
   environment: process.env.CITY
 });
 
+const s3 = new AWS.S3({ region: process.env.REGION });
+
+const levels = JSON.parse(fs.readFileSync(`${__dirname}/levels.json`));
+const getLevel = aqi => levels[String(Math.max(...Object
+  .keys(levels)
+  .map(value => parseInt(value, 10))
+  .filter(value => value <= aqi)))];
+
 module.exports.cron = rollbar.wrap(async () => {
-  const data = await request({
+  const currentData = await request({
     method: 'GET',
-    uri: `http://api.waqi.info/feed/${process.env.CITY}/?token=${process.env.WAQI_TOKEN}`,
-    json: true
+    uri: `http://api.waqi.info/feed/${process.env.CITY}/?token=${process.env.WAQI_TOKEN}`
   });
-  return data;
+  const current = JSON.parse(currentData);
+
+  const s3Key = `${process.env.CITY}-last-reading.json.gz`;
+
+  let previousData = "{}";
+  try {
+    previousData = zlib.gunzipSync((await s3.getObject({
+      Bucket: process.env.DATA_BUCKET_NAME,
+      Key: s3Key
+    }).promise()).Body).toString("utf8");
+  } catch (e) {
+    if (e.code !== 'NoSuchKey') {
+      throw e;
+    }
+  }
+  const previous = JSON.parse(previousData);
+
+  if (currentData !== previousData) {
+    // update previous data
+    await s3.putObject({
+      ContentType: "application/json",
+      ContentEncoding: "gzip",
+      Body: zlib.gzipSync(currentData, { level: 9 }),
+      Bucket: process.env.DATA_BUCKET_NAME,
+      Key: s3Key
+    }).promise();
+
+    const prevLevel = getLevel(get(previous, "data.aqi", 0));
+    const curLevel = getLevel(get(current, "data.aqi", 0));
+    if (prevLevel !== curLevel) {
+      // todo: notification
+    }
+  }
+
+  return current;
 });
